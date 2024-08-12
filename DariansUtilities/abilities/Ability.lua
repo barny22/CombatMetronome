7ca890568da9e233d04e6b584b9b20f7f671cf69
barny22 <sebastian.wick@outlook.com>
2024-08-12 15:43:25 +0200

    Reworked ability.lua
    This should fix bugs with scribed skill elemental explosion and bug with abilities not being triggered

--- DariansUtilities/abilities/Ability.lua
+++ DariansUtilities/abilities/Ability.lua
@@ -18,7 +18,6 @@ local TargetGround = {
 local log = Util.log
 
 function Ability:ForId(id)
-    -- local APIVersion = GetAPIVersion()
 	local o = self.cache[id]
 	if (o) then 
         -- d(" Ability "..o.name.." is cached for id, "..id)
@@ -40,12 +39,7 @@ function Ability:ForId(id)
     -- end
 
     o.id = id
-    o.name = Ability:CropZOSSpellName(GetAbilityName(id))
-    -- if APIVersion < 101042 then
-    -- o.channeled, 
-    -- o.castTime, 
-    -- o.channelTime = GetAbilityCastInfo(id)                                                   --- this was pre API 101042
-    -- if o.channelTime == nil then
+    o.name = Ability:CropZOSString(GetAbilityName(id))
     local channeled, duration = GetAbilityCastInfo(id)
     o.channeled = channeled
     if o.channeled then
@@ -57,8 +51,8 @@ function Ability:ForId(id)
     end
     -- end
     o.delay = math.max(o.castTime, o.channelTime)
-    o.instant = not (o.castTime > 0 or o.channelTime > 0)
-    o.casted = not (o.instant or o.channeled)
+    o.instant = not (o.channelTime > 0 or o.castTime > 0)
+    o.casted = not o.channeled
     -- o.passive = passive
     o.target = GetAbilityTargetDescription(id)
 	o.icon = GetAbilityIcon(id)
@@ -113,11 +107,11 @@ end
     -- return self:ForId(Ability.getIdFromName(name))
 -- end
 
-function Ability:CropZOSSpellName(zosString)
-    local _, zosSpellDivider = string.find(zosString, "%^")
+function Ability:CropZOSString(zosString)
+    local _, zosStringDivider = string.find(zosString, "%^")
     
-    if zosSpellDivider then
-        return string.sub(zosString, 1, zosSpellDivider - 1)
+    if zosStringDivider then
+        return string.sub(zosString, 1, zosStringDivider - 1)
     else
         return zosString
     end
@@ -126,7 +120,7 @@ end
 -- function Ability.getIdFromName(name)
     -- local hotbar = GetActiveHotbarCategory()
     -- for i = 1, 300000 do
-        -- if (CanAbilityBeUsedFromHotbar(i, hotbar) and name == Ability:CropZOSSpellName(GetAbilityName(i))) then
+        -- if (CanAbilityBeUsedFromHotbar(i, hotbar) and name == Ability:CropZOSString(GetAbilityName(i))) then
             -- return i
         -- end
     -- end
@@ -145,7 +139,7 @@ Ability.Tracker.name = "Util.Ability.Tracker"
 
 local EVENT_RECORD_DELAY = 10
 local EVENT_FORCE_WAIT = 100
-local DISMOUNT_PERIOD = 1000
+local DISMOUNT_PERIOD = 300
 
 function Ability.Tracker:Start()
     if self.started then return end
@@ -153,9 +147,12 @@ function Ability.Tracker:Start()
     -- d("Abiilty Tracker Started!")
 
     self.started = true
+    
     self.callLaterActivated = false
+    self.gcdEnd = 0
     
     self.slotCounter = 0
+    self.lastQueuedEvent = {}
 
     self.log = false
     self.mountedState = IsMounted()
@@ -197,11 +194,21 @@ end
 function Ability.Tracker:Update()
     local time = GetFrameTimeMilliseconds()
     
+    -- if self.queuedEvent ~= self.lastQueuedEvent then
+        -- if self.queuedEvent then
+            -- d(self.queuedEvent.ability.name.." is queued")
+        -- else
+            -- d("Queued Event was set to nil")
+        -- end
+    -- end
+    -- self.lastQueuedEvent = self.queuedEvent
+    
     -- Fire off late events if no SLOT_UPDATE events
     if (not self.eventStart and self.queuedEvent and self.queuedEvent.allowForce) then
         if (time > self.queuedEvent.recorded + EVENT_FORCE_WAIT) then
             -- _=self.log and d("Event force "..tostring(time - self.queuedEvent.recorded).."ms ago")
             self.eventStart = self.queuedEvent.recorded
+            -- d("fired late event")
             self:AbilityUsed()
         end
     end
@@ -259,12 +266,15 @@ function Ability.Tracker:NewEvent(ability, slot, start)
     event.hotbar = GetActiveHotbarCategory()
 
     self.queuedEvent = event
+    -- if self.queuedEvent.allowForce then
+        -- d(self.queuedEvent.ability.name.." was the  event")
+    -- end
 
     -- d("  Allow force = "..tostring(self.queuedEvent.allowForce))
 end
 
 function Ability.Tracker:CancelEvent()
-    -- self.eventStart = nil
+    self.eventStart = nil
     self.queuedEvent = nil
 
     if (self.currentEvent) then
@@ -285,6 +295,7 @@ function Ability.Tracker:AbilityUsed()
     event.start = self.eventStart
     self.queuedEvent = nil
     self:CallbackAbilityUsed(event)
+    -- d(event.ability.name.." was used")
 
     if (event.ability.instant or event.ability.channeled) then
         self:CallbackAbilityActivated(event)
@@ -326,17 +337,27 @@ function Ability.Tracker:HandleSlotUpdated(e, slot)
     local time = GetFrameTimeMilliseconds()
     local remaining, duration, global, t = GetSlotCooldownInfo(slot)
     
+    if not global then return end
+    
     ----------------------------------------
     ---- Bugfix for elemental explosion ----
     ----------------------------------------
     self.slotCounter = self.slotCounter + 1
-            
-    if self.slotCounter == 5 and remaining == 0 and duration == 0 and not self.callLaterActivated then
+     
+    self.buggedSkill = remaining == 0 and duration == 0
+    local callbackName = ""
+    if self.buggedSkill then
+        callbackName = "Buggy"
+    else
+        callbackName = "Fake"
+    end
+    
+    if self.slotCounter == 5 --[[and remaining == 0 and duration == 0 ]]and not self.callLaterActivated then
         -- d("call later activated")
         self.callLaterActivated = true
         -- self.timeActivated = time
-        EVENT_MANAGER:RegisterForUpdate(                                                                                          -- for whatever reason zo_callLater always returns a bug here, so I decided to just code it manually
-            self.name.."HandleSlotUpdatedCallback",
+        EVENT_MANAGER:RegisterForUpdate(                                                                                          -- I want to be able to reset these manually, that's why they are coded seperatly not using zo_callLater()
+            self.name.."HandleSlotUpdatedCallback"..callbackName,
             1000/60,
             function()
                 Ability.Tracker:TrackGCD()
@@ -346,12 +367,12 @@ function Ability.Tracker:HandleSlotUpdated(e, slot)
     
     if self.callLaterActivated then
         EVENT_MANAGER:RegisterForUpdate(
-            self.name.."ResetHandleSlotUpdatedCallback",
+            self.name.."ResetHandleSlotUpdatedCallback"..callbackName,
             170,
             function()
                 if self.callLaterActivated then
-                    EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallback")
-                    EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallback")
+                    EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallback"..callbackName)
+                    EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallback"..callbackName)
                     self.callLaterActivated = false
                     -- d("call later reset by waiting")
                 end
@@ -363,19 +384,23 @@ function Ability.Tracker:HandleSlotUpdated(e, slot)
     ---- Bugfix end ----
     --------------------
     
-    local abilityUsed = (duration > 0 and remaining > 0 and not self.mountedState and not ArePlayerWeaponsSheathed())              --more specified triggers for abilities
+    local abilityUsed = (duration > 0 and remaining > 0 and not self.mountedState and not ArePlayerWeaponsSheathed() and time > self.gcdEnd and self.queuedEvent)              --more specified triggers for abilities
     
     if abilityUsed then
+        -- d("Ability used was triggered")
     
-        self.gcd = remaining or self.queuedEvent.ability.delay
+        self.gcdEnd = remaining + GetFrameTimeMilliseconds()
 
         local oldStart = self.eventStart or 0
-        self.eventStart = time + remaining - duration 
+        self.eventStart = time + remaining - duration
+        
+        -- if self.queuedEvent then d("Ability used + queuedEvent") end
+        -- if self.eventStart > oldStart then d("Ability used + new start") end
         
         -- if (oldStart ~= self.eventStart) then
             -- _=self.log and d(""..time.." : Event start "..tostring(duration - remaining).."ms ago")
         -- end
-        if self.queuedEvent and --[[self.queuedEvent.triggerOnSlotUpdated and]] self.eventStart > oldStart and abilityUsed then
+        if --[[self.queuedEvent and self.queuedEvent.triggerOnSlotUpdated and]] self.eventStart > oldStart then
             -- _=self.log and d(""..time.." : Moved queued "..self.queuedEvent.ability.name.." to current") 
             -- log("  Dispatching ", self.queuedEvent.ability.name)
             -- log("    oldStart = ", oldStart)
@@ -392,7 +417,7 @@ function Ability.Tracker:HandleSlotUsed(e, slot)
     
     local ability = {}
     local actionType = GetSlotType(slot)
-    if actionType == ACTION_TYPE_CRAFTED_ABILITY then --3 then
+    if actionType == ACTION_TYPE_CRAFTED_ABILITY then
         -- d("Crafted ability executed")
         ability = Util.Ability:ForId(GetAbilityIdForCraftedAbilityId(GetSlotBoundId(slot)))
         -- d("Ability used - ", ability.name)
@@ -411,14 +436,16 @@ function Ability.Tracker:HandleSlotUsed(e, slot)
     if (ability.light) then return end
 
     self:CancelEvent()
+    -- d("Canceled Event because of |ce11212new event|")
 
     if (ability.heavy) then return end
 
-    -- _=self.log and d(""..getFrameTimeMilliseconds().." : New ability - "..ability.name)
-    self:NewEvent(ability, slot)
+    -- _=self.log and
+    -- d(""..GetFrameTimeMilliseconds()..": New ability - "..ability.name)
+    self:NewEvent(ability, slot, GetFrameTimeMilliseconds())
 end
 
-local CHECK_PLAYER_UNIT_ID_INTERVAL = 5000
+-- local CHECK_PLAYER_UNIT_ID_INTERVAL = 5000
 
 --                                      (a)bility | (d)amage | (p)ower | (t)arget | (s)ource | (h)it
 --                                      ------------------------------------------------------------
@@ -432,6 +459,7 @@ function Ability.Tracker:HandleCombatEvent(_,     res,  err,   aName, _, _,    s
             or res == ACTION_RESULT_STAGGERED
             or res == ACTION_RESULT_STUNNED
             or res == ACTION_RESULT_INTERRUPTED) then
+            -- d("Canceled event because of |ce11212CC|")
             self:CancelEvent()
             return
         end
@@ -443,8 +471,9 @@ function Ability.Tracker:HandleCombatEvent(_,     res,  err,   aName, _, _,    s
     if (Util.Targeting.isUnitPlayer(sName, sUId)) then
         -- log("Source is player")
 
-        if (res == COMBAT_RESULT_CANNOT_USE and not self.abilityAllowForce) then
+        if (res == COMBAT_RESULT_CANNOT_USE and self.currentEvent and not self.currentEvent.allowForce) then
             self:CancelEvent()
+            -- d("Canceled event because of |ce11212result|")
             return
         end
 
@@ -481,23 +510,38 @@ function Ability.Tracker:TrackGCD()
     end
     local gcdProgress = slotRemaining/slotDuration
     
-    if self.barswap and self.callLaterActivated then                                                            -- unfortunately barswap triggers after slot update, so i have to reset here instead of not tracking gcd...
-            EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallback")
-            EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallback")
-            self.callLaterActivated = false
-            self.barswap = false
-            -- d("Call later was reset because of barswap")
-            -- local deactivationTime = GetFrameTimeMilliseconds() - self.timeActivated
-            -- d("time to deactivate by was: "..deactivationTime)
-    elseif gcdProgress > 0 and self.callLaterActivated then
-            EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallback")
-            EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallback")
-            self.callLaterActivated = false
-            -- d("Call later was reset by trigger")
-            -- local deactivationTime = GetFrameTimeMilliseconds() - self.timeActivated
-            -- d("time to deactivate by was: "..deactivationTime)
-            for i = 3,4 do
-                Ability.Tracker:HandleSlotUpdated(_, i)
-            end
+    if self.barswap and self.callLaterActivated and self.buggedSkill then                                                            -- unfortunately barswap triggers after slot update, so i have to reset here instead of not tracking gcd...
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallbackBuggy")
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallbackBuggy")
+        self.callLaterActivated = false
+        self.barswap = false
+        -- d("Call later was reset because of barswap")
+        -- local deactivationTime = GetFrameTimeMilliseconds() - self.timeActivated
+        -- d("time to deactivate by was: "..deactivationTime)
+    elseif self.barswap and self.callLaterActivated and not self.buggedSkill then
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallbackFake")
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallbackFake")
+        self.callLaterActivated = false
+        self.barswap = false
+        -- d("Call later was reset because of barswap")
+        -- local deactivationTime = GetFrameTimeMilliseconds() - self.timeActivated
+        -- d("time to deactivate by was: "..deactivationTime)
+    elseif gcdProgress > 0 and self.callLaterActivated and self.buggedSkill then
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallbackBuggy")
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallbackBuggy")
+        self.callLaterActivated = false
+        -- d("Call later was reset by trigger")
+        -- local deactivationTime = GetFrameTimeMilliseconds() - self.timeActivated
+        -- d("time to deactivate by was: "..deactivationTime)
+        for i = 3,4 do
+            Ability.Tracker:HandleSlotUpdated(_, i)
+        end
+    elseif not self.buggedSkill and self.callLaterActivated and gcdProgress == 0 then
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."HandleSlotUpdatedCallbackFake")
+        EVENT_MANAGER:UnregisterForUpdate(self.name.."ResetHandleSlotUpdatedCallbackFake")
+        self.callLaterActivated = false
+        -- d("cancelling Event at "..GetFrameTimeMilliseconds().." because it was |ce11212fake|")
+        self:CancelEvent()
+        if self.gcdEnd > GetFrameTimeMilliseconds() then self.gcdEnd = GetFrameTimeMilliseconds() end
     end
 end
