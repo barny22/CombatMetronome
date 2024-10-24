3b8821c9deaa137329e6c6a5e7ccdb7c98803ebe
barny22 <158307262+barny22@users.noreply.github.com>
2024-10-24 14:23:10 +0200

    Update upload_to_esoui.py

--- .github/scripts/upload_to_esoui.py
+++ .github/scripts/upload_to_esoui.py
@@ -1,26 +1,42 @@
 import requests
 import argparse
+from contextlib import closing
 
-def upload_addon(api_token, addon_id, version, file_path, changelog, compatible, description, test_only):
+def upload_addon(api_token, addon_id, version, file_path, changelog_path, compatible, description_path, test_only):
     url = "https://api.esoui.com/addons/updatetest" if test_only else "https://api.esoui.com/addons/update"
     
     headers = {
         "x-api-token": api_token
     }
 
-    files = {
-        "updatefile": open(file_path, "rb"),
-        "changelog": changelog,
-        "compatible": compatible,
-        "description": description
-    }
+    try:
+        with closing(open(file_path, "rb")) as updatefile, \
+             closing(open(changelog_path, "rb")) as changelog_file, \
+             closing(open(description_path, "rb")) as description_file:
+            
+            files = {
+                "updatefile": updatefile,
+                "changelog": changelog_file,
+                "compatible": compatible,
+                "description": description_file
+            }
 
-    response = requests.post(url, headers=headers, files=files)
-    print(response.json())
+            response = requests.post(url, headers=headers, files=files)
+            response.raise_for_status()  # Überprüfe auf HTTP-Fehler
+            print(response.json())
+
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
 
 def read_file(file_path):
-    with open(file_path, 'r') as file:
-        return file.read()
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except Exception as e:
+        print(f"Error reading file {file_path}: {e}")
+        exit(1)  # Beende das Skript bei einem Fehler
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Upload an ESO Addon to ESOUI")
@@ -36,8 +52,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    changelog = read_file(args.changelog_path)
-    description = read_file(args.description_path)
+    print(f"Parsed arguments: {args}")  # Zum Debuggen
 
     # Den Aufruf der Funktion mit den Argumenten
-    upload_addon(args.api_token, args.addon_id, args.version, args.file_path, changelog, args.compatible, description, args.test_only.lower() == 'true')
+    upload_addon(
+        api_token=args.api_token,
+        addon_id=args.addon_id,
+        version=args.version,
+        file_path=args.file_path,
+        changelog_path=args.changelog_path,
+        compatible=args.compatible,
+        description_path=args.description_path,
+        test_only=args.test_only.lower() == 'true'
+    )
