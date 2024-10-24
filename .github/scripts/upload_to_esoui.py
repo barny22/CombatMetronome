c95b97ff1f8243067ba3b898f4a60bdef36efddb
barny22 <158307262+barny22@users.noreply.github.com>
2024-10-24 14:08:08 +0200

    Update upload_to_esoui.py

--- .github/scripts/upload_to_esoui.py
+++ .github/scripts/upload_to_esoui.py
@@ -2,7 +2,6 @@ import requests
 import argparse
 
 def upload_addon(api_token, addon_id, version, file_path, changelog, compatible, description, test_only):
-    # Bestimme die URL basierend auf dem test_only Flag
     url = "https://api.esoui.com/addons/updatetest" if test_only else "https://api.esoui.com/addons/update"
     
     headers = {
@@ -19,20 +18,26 @@ def upload_addon(api_token, addon_id, version, file_path, changelog, compatible,
     response = requests.post(url, headers=headers, files=files)
     print(response.json())
 
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
 if __name__ == "__main__":
-    # Argumente definieren
     parser = argparse.ArgumentParser(description="Upload an ESO Addon to ESOUI")
     
     parser.add_argument("--api_token", required=True, help="API token for ESOUI")
     parser.add_argument("--addon_id", required=True, help="ID of the addon")
     parser.add_argument("--version", required=True, help="Version of the addon")
     parser.add_argument("--file_path", required=True, help="Path to the addon ZIP file")
-    parser.add_argument("--changelog", required=True, help="Changelog for the addon")
+    parser.add_argument("--changelog_path", required=True, help="Path to the changelog file")
     parser.add_argument("--compatible", required=True, help="Compatible Patch ID")
-    parser.add_argument("--description", required=True, help="Description of the addon")
+    parser.add_argument("--description_path", required=True, help="Path to the description file")
     parser.add_argument("--test_only", required=True, help="Use test URL if set to true")
 
     args = parser.parse_args()
 
+    changelog = read_file(args.changelog_path)
+    description = read_file(args.description_path)
+
     # Den Aufruf der Funktion mit den Argumenten
-    upload_addon(args.api_token, args.addon_id, args.version, args.file_path, args.changelog, args.compatible, args.description, args.test_only.lower() == 'true')
+    upload_addon(args.api_token, args.addon_id, args.version, args.file_path, changelog, args.compatible, description, args.test_only.lower() == 'true')
