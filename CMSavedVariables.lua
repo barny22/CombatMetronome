ba7223768723dbdb7ab3e74e15004befcb4710ea
barny22 <158307262+barny22@users.noreply.github.com>
2025-02-13 18:58:04 +0100

    Update CMSavedVariables.lua
    Reverted changes to reticleStamMag

--- CMSavedVariables.lua
+++ CMSavedVariables.lua
@@ -89,7 +89,6 @@ function CombatMetronome:ConvertSavedVariables(oldSV)
 	self.SV.Resources.showResourcesForGuard = configCache.showResourcesForGuard
 	self.SV.Resources.hpHighlightThreshold = configCache.hpHighlightThreshold
 	self.SV.Resources.reticleHp = configCache.reticleHp
-	self.SV.Resources.reticleMagStam = configCache.reticleMagStam
 	
 	self.SV.StackTracker = { }
 	self.SV.StackTracker.isUnlocked = configCache.trackerIsUnlocked
